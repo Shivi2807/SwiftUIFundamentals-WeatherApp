3d2d265b65c649184a4a4ba12c7dede4ab61ecc2
Shivi1 <sagarwal2896@gmail.com>
2022-03-23 13:13:36 +0530

    Initial Commit

---
+++ Shared/SwiftUI_TableViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TableViewApp.swift
+//  Shared
+//
+//  Created by Shivi Agarwal on 23/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TableViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
